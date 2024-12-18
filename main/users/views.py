c4f7cbceb8691d2acc927848833185e2a1b7b6f4
qwerty701 <58883600+qwerty701@users.noreply.github.com>
2024-12-18 09:32:26 +0300

    Update views.py

--- main/users/views.py
+++ main/users/views.py
@@ -113,7 +113,7 @@ class UserCartView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Home - Корзина'
+        context['title'] = 'Hello World - Корзина'
         return context
 
 @login_required
